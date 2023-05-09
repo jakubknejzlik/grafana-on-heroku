34dadb687dfd5ae5b07c8eae64b9d4e7de64709c
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2023-05-09 16:20:48 +0200

    Update database config to mysql

--- heroku-run.sh
+++ heroku-run.sh
@@ -1,3 +1,4 @@
 sed -i -e 's|<HTTP_PORT>|'$PORT'|' /etc/grafana/grafana.ini
 sed -i -e 's|<DATABASE_URL>|'$DATABASE_URL'|' /etc/grafana/grafana.ini
+sed -i -e 's|<CLEARDB_DATABASE_URL>|'$CLEARDB_DATABASE_URL'|' /etc/grafana/grafana.ini
 /run.sh
